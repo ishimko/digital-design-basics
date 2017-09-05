e37fe1d7396dbe8c91a2c37aece836f259d4d3cd
vanashimko <vana.shimko@gmail.com>
2017-09-05 22:37:56 +0300

    Add example task

---
+++ digital_design_basics/src/task1.vhd
@@ -0,0 +1,15 @@
+LIBRARY ieee;      				-- Use IEEE library 
+USE ieee.std_logic_1164.all;    -- IEEE packages 
+ 
+ENTITY mux IS      				-- Entity of multiplexer 
+   port(        				-- Ports description 
+      A,B,S: in std_logic;    	-- A and B are logical inputs 
+        						-- S is the control input signal 
+      Z: out std_logic      	-- Z is a logical output 
+   ); 
+END mux; 
+-- 
+ARCHITECTURE beh OF mux IS  	-- Architecture of multiplexer 
+BEGIN 
+   Z<=A when S='0' else B;    	-- Behavioural description by logic equation 
+END beh;
