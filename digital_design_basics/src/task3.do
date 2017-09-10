20cb58997290715241b007d52528859e739afc58
vanashimko <vana.shimko@gmail.com>
2017-09-10 11:39:47 +0300

    Add task 3

---
+++ digital_design_basics/src/task3.do
@@ -0,0 +1,3 @@
+force -freeze -r 100.000000 ns {/fun/in1} 0 0 ns, 1 50 ns
+force -freeze -r 50.000000 ns {/fun/in2} 0 0 ns, 1 25 ns
+force -freeze -r 25000.000000 ps {/fun/in3} 0 0 ps, 1 12 ps
---
+++ digital_design_basics/src/task3.vhd
@@ -0,0 +1,14 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+ 
+ENTITY fun IS      					
+   port(        					
+      in1, in2, in3: in std_logic; 
+      Q: out std_logic
+   ); 
+END fun;
+-- 
+ARCHITECTURE beh OF fun IS
+BEGIN 
+   Q<=(in1 and in2) or (in3 and not in2);
+END beh;
